8910d722c50c0cd346df3659ed1d208d337f4975
veershah04 <144481659+veershah04@users.noreply.github.com>
2024-10-24 15:33:32 +0530

    Update settings.py

--- Expense Tracker/expensetracker/settings.py
+++ Expense Tracker/expensetracker/settings.py
@@ -84,14 +84,10 @@ WSGI_APPLICATION = 'expensetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
